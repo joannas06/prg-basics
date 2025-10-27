326c6a268964cd6af68b5805d4cf8519b003c349
Joanna Strzesniewska <j.strzesniewska88@gmail.com>
2025-10-27 18:08:48 +0100

    tasks done 27.10

---
+++ 02-TypesAndVariables/3-6.py
@@ -0,0 +1,15 @@
+# Program calculating distance from the horizon
+
+import math
+
+h1 = 1.7
+
+d1 = 3.57 * math.sqrt(h1)
+
+print ("This is the distance from the horizon: " ,d1 ,"km")
+
+h2 = 20
+
+d2 = 3.57 * math.sqrt(h2)
+
+print ("This is the distance from the horizon from the hotel room: " ,d2 , "km")
---
+++ 02-TypesAndVariables/4-1.py
@@ -0,0 +1,10 @@
+###
+# Printing student's personal data
+#
+name = "Adam"
+age = 18
+height = 180
+age2 = 24
+print(f"My name is {name}.")
+print(f"I am {age} years old, and my height is {height} cm.")
+print(f"In 6 years I will be {age2} years old")
