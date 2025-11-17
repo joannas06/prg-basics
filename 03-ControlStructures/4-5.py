fb7065f00b8279ba0d5c2ce3f54771848c7ad770
Joanna Strzesniewska <j.strzesniewska88@gmail.com>
2025-11-17 16:16:44 +0100

    ex.4 in pg 3

---
+++ 03-ControlStructures/4-5.py
@@ -0,0 +1,20 @@
+###
+# Encrypts text using Caesar Code, shifting each letter
+# in the alphabet right one position
+#
+plain_text = 'The early bird catches the worm'
+encrypted_text = ''
+
+for char in plain_text:
+    # read the character's code (use ord())
+    ord(char)
+    # add one to the character's code
+    ord(char + 1)
+    # replace new character code with its
+    # corresponding character (use chr())
+    ...
+    # add encrypted character to encrypted text
+    ...
+
+print(plain_text...)
+print(encrypted_text...)
