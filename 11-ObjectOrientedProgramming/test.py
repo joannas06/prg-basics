0bc528a5592fe9eaba8cf622c7edea87e971a6a7
Joanna strzesniewska <j.strzesniewska88@gmail.com>
2025-12-08 16:23:39 +0100

    teraz done oop

--- 11-ObjectOrientedProgramming/test.py
+++ 11-ObjectOrientedProgramming/test.py
@@ -7,6 +7,9 @@ class Car:
    def display_info(self):
        print(f"Car: {self.brand} {self.model}, Year: {self.year}")
 
+   def __str__(self):
+       return f"{self.brand} {self.model}"
+
 
 car1 = Car('Fiat','Tipo',2024)
 car2 = Car('Polonez','Karo',2003)
@@ -14,3 +17,4 @@ car1.display_info()
 car2.display_info()
 #lub
 print(car1.brand) #prints only the brand
+print(car1)
